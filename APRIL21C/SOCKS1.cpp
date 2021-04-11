fa06a36ca167c4a0c28683347a9beb6f2018ee89
Dhruba Jyoti Chakraborty <61685945+dhrubajyoti89@users.noreply.github.com>
2021-04-11 21:43:33 +0530

    Update SOCKS1.cpp

--- APRIL21C/SOCKS1.cpp
+++ APRIL21C/SOCKS1.cpp
@@ -1,1 +1,31 @@
+// Problem: https://www.codechef.com/APRIL21C/problems/SOCKS1
+// Codechef April Long Challenge 2021 (APRIL21C SOLUTIONS)
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, flag = 0;
+    cin >> a >> b >> c;
+    int arr[11] = {0};
+    arr[a]++;
+    arr[b]++;
+    arr[c]++;
+    for (int i = 1; i < 11; i++)
+    {
+        if (arr[i] > 1)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+        cout << "NO";
+    else
+        cout << "YES";
+
+    cout << endl;
+
+    return 0;
+}
